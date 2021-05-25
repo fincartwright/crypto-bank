b105838f5aaa3795ac699d7117855d1520fd5ca7
BOBENSTIEN <fin.cartwright123@gmail.com>
2021-05-25 16:40:37 +0100

    updated

---
+++ index.js
@@ -0,0 +1,14 @@
+const bitcoinButton = document.getElementById('btc');
+
+function buyCrypto(name) {
+    switch (name) {
+         case 'btc': {   
+        
+        prompt('bitcoin button pressed')
+    }
+  }
+}
+
+bitcoinButton.onclick() = function () {
+  buyCrypto('btc')
+};
