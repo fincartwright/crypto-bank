272c996bc2ac95875cb57aee7d269157860b08a4
BOBENSTIEN <fin.cartwright123@gmail.com>
2021-06-07 16:00:55 +0100

    updated js

--- index.js
+++ index.js
@@ -18,42 +18,32 @@ case 'ethereum':{
   break;
 }
 }}
-// let state = {
-//   inTheBank: 100000, 
-//   cryptoCurrencies: [
-//     {
-//       name: 'bitcoin',
-//       price: 20000,
-//       holding: 0,
-//     },
-//     {
-//       name: 'doge',
-//       price: 1,
-//       holding: 0,
-//     },
-//     {
-//       name: 'ethereum',
-//       price: 200,
-//       holding: 0,
-//     }
-//   ]
+ let state = {
+  inTheBank: 100000, 
+   cryptoCurrencies: [
+     {
+       name: 'bitcoin',
+      price: 20000,
+       holding: 0,
+     },
+     {
+       name: 'doge',
+       price: 1,
+       holding: 0,
+     },
+     {
+       name: 'ethereum',
+       price: 200,
+       holding: 0,
+     }
+   ]
   
-// }
-
-
-
-
-   
-    
-
-  
-
-
+ }
 
  function makePayment (amount, name) {
  const cryptoCurrencyDetails = state.cryptoCurrencies.find(function (crypto){
    return crypto.name === name;
  });
- console.log(cryptoCurrencyDetails)
+ console.log(cryptoCurrencyDetails);
  }
 
