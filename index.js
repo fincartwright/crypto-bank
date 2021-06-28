97297c7e9a1a6c36c264f31710deda8e52300f9f
BOBENSTIEN <fin.cartwright123@gmail.com>
2021-06-28 18:40:20 +0100

    updated

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
 let state = {
-  inTheBank: 100000, 
-   cryptoCurrencies: [
-     {
-       name: 'bitcoin',
+  inTheBank: 100000,
+  cryptoCurrencies: [
+    {
+      name: 'bitcoin',
       price: 20000,
-       holding: 0,
-     },
-     {
-       name: 'doge',
-       price: 1,
-       holding: 0,
-     },
-     {
-       name: 'ethereum',
-       price: 200,
-       holding: 0,
-     }
-   ] 
- }
+      holding: 0,
+    },
+    {
+      name: 'doge',
+      price: 1,
+      holding: 0,
+    },
+    {
+      name: 'ethereum',
+      price: 200,
+      holding: 0,
+    }
+  ]
+}
 
- function buyCrypto(name) {
-  switch(name) {
-    case 'bitcoin':{
+function buyCrypto(name) {
+  switch (name) {
+    case 'bitcoin': {
       const amount = prompt('How much Bitcoin would you like to purchase?')
+      this.makePayment(JSON.parse(amount), name);
+      break;
+    }
+    case 'doge': {
+      const amount = prompt('How much Doge would you like to purchase?')
       this.makePayment(amount, name);
       break;
     }
-  case 'doge':{
-    const amount = prompt('How much Doge would you like to purchase?')
+    case 'ethereum': {
+      const amount = prompt('How much Ethereum would you like to purchase?')
       this.makePayment(amount, name);
-    prompt('doge button pressed')
-    break;
+      break;
+    }
   }
-case 'ethereum':{
-  const amount = prompt('How much Ethereum would you like to purchase?')
-      this.makePayment(amount, name);
-  prompt('ethereum button pressed')
-  break;
-  }
- }
 }
 
-function makePayment (amount, name) {
-  const cryptoCurrencyDetails = state.cryptoCurrencies.find(function (crypto){
+function makePayment(amount, name) {
+  const cryptoCurrencyDetails = state.cryptoCurrencies.find(function (crypto) {
     return crypto.name === name;
   });
 
- const coinsICanBuy = amount / cryptoCurrencyDetails.price; 
- if( amount > state.inTheBank) {
-  alert('you cannot afford that')
- }
- else{
- console.log(coinsICanBuy)
- state.inTheBank = state.inTheBank - amount,
- console.log(state.inTheBank)
- const inMyBank = document.querySelector('p');
- inMyBankHTML.innerHTML = ""
- }
+  cryptoCurrencyDetails.holding = cryptoCurrencyDetails.holding + (amount / cryptoCurrencyDetails.price);
+  if (amount > state.inTheBank) {
+    alert('you cannot afford that')
+  }
+  else {
+    state.inTheBank = state.inTheBank - amount;
+    const inMyBankHTML = document.querySelector('.in-my-bank span');
+    const coinAmount = document.querySelector("#" + cryptoCurrencyDetails.name + " span");
+    inMyBankHTML.innerHTML = state.inTheBank;
+    coinAmount.innerHTML = cryptoCurrencyDetails.holding;
+
+  }
 }
 
